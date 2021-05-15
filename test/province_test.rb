66ef498457435431428c573fd1884e4e22d55a1a
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-14 22:02:09 -0700

    Test province shortfall raises NoMethodError when demand is an empty string

--- test/province_test.rb
+++ test/province_test.rb
@@ -6,60 +6,68 @@ require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
 
-  def test_province_shortfall
+  def test_shortfall
     asia = Province.new(sample_province_data)
     assert_equal(5, asia.shortfall)
   end
 
-  def test_province_profit
+  def test_profit
   	asia = Province.new(sample_province_data)
   	assert_equal(230, asia.profit)
   end
 
-  def test_province_shortfall_after_production_change
+  def test_shortfall_after_production_change
   	asia = Province.new(sample_province_data)
   	asia.producers[0].production = 20
   	assert_equal(-6, asia.shortfall)
   end
 
-   def test_province_profit_after_production_change
+   def test_profit_after_production_change
   	asia = Province.new(sample_province_data)
   	asia.producers[0].production = 20
   	assert_equal(292, asia.profit)
   end
 
-  def test_province_shortfall_with_no_producers
+  def test_shortfall_with_no_producers
   	no_producers = Province.new(sample_province_data_no_producers)
   	assert_equal(30, no_producers.shortfall)
   end
 
-  def test_province_profit_with_no_producers
+  def test_profit_with_no_producers
   	no_producers = Province.new(sample_province_data_no_producers)
   	assert_equal(0, no_producers.profit)
   end
 
-  def test_province_shortfall_with_0_demand
+  def test_shortfall_with_0_demand
   	asia = Province.new(sample_province_data)
   	asia.demand = 0
   	assert_equal(-25, asia.shortfall)
   end
 
-  def test_province_profit_with_0_demand
+  def test_profit_with_0_demand
   	asia = Province.new(sample_province_data)
   	asia.demand = 0
   	assert_equal(0, asia.profit)
   end
 
-  def test_province_shortfall_with_negative_demand
+  def test_shortfall_with_negative_demand
   	asia = Province.new(sample_province_data)
   	asia.demand = -1
   	assert_equal(-26, asia.shortfall)
   end
 
-  def test_province_profit_with_negative_demand
+  def test_profit_with_negative_demand
   	asia = Province.new(sample_province_data)
   	asia.demand = -1
   	assert_equal(-10, asia.profit)
+  end
+
+  def test_shortfall_with_empty_string_demand
+  	assert_raises NoMethodError do
+  	  asia = Province.new(sample_province_data)
+  	  asia.demand = ""
+  	  asia.shortfall
+	end
   end
 
 end
