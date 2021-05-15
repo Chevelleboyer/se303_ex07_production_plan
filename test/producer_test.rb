01a57226f39e19cb0a2c1185ab4a6b7e42dae3e8
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-14 21:22:55 -0700

    Implement and pass test that changes producer production

--- test/producer_test.rb
+++ test/producer_test.rb
@@ -2,7 +2,15 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/producer'
+require_relative '../lib/province'
+require_relative '../lib/data'
 
 class ProducerTest < Minitest::Test
+
+	def test_change_production
+		byzantium = Producer.new(Province.new(sample_province_data), {name: 'Byzantium', cost: 10, production: 9})
+		byzantium.production = 20
+		assert_equal(20, byzantium.production)
+	end
 
 end
